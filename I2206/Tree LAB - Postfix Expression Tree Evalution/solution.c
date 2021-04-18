7dcac37bb6f1afa91472516b0ac0533804ca2123
OmarKhatib <elkhatibomar@outlook.com>
2021-04-18 15:13:40 +0300

    refactored solution with much less repetitive code

--- I2206/Tree LAB - Postfix Expression Tree Evalution/solution.c
+++ I2206/Tree LAB - Postfix Expression Tree Evalution/solution.c
@@ -1,49 +1,32 @@
 // Question is to implement following video https://www.youtube.com/watch?v=xSR38kgzyiQ
 // code in picture 1 and 2 are already giving
+// operation function provided by Dr give segmentation FAULT maybe a Bug, so Implementing one will make this pass.
 
-ExpressionTree Build(char *postfix)
-{
-    stack s = CreateStack();
+ExpressionTree returnPop(stack *s) {
     element e;
-    element1 currChar;
-
-    // your code here
-    for(int i=0 ; *(postfix+i) != '\0' ; i++) {
-        currChar = *(postfix+i);
-
-        if(is_digit(currChar)) {
-            Push(&s , Construct(currChar , NULL , NULL));
-        }
-        else if(currChar == '#') {
-            Top(s , &e);
-            Pop(&s);
-            Push(&s , Construct(currChar , NULL , e));
-        } else {
-            Top(s , &e);
-            Pop(&s);
-
-            element temp = e;
-
-            Top(s , &e);
-            Pop(&s);
-
-            Push(&s , Construct(currChar , e , temp));
-        }
-    }
-    Top(s , &e);
-    Pop(&s);
-
+    Top(&t , &e);
+    pop(&s);
     return e;
 }
 
-float result(ExpressionTree B)
-{
+ExpressionTree Build(char *postfix) {
+    stack s = CreateStack();
+    element left , right;
+
+    for(int i = 0 ; *(postfix+i) != '\0'; i++) {
+        left = right = NULL;
+        if(is_operator( *(postfix+i) ) ) {
+            if( *(postfix+i) != '#') left = returnPop(&s);
+            right = returnPop(&s);
+        }
+        Push(&s , Construct(*(postfix+i) , left , right) );
+    }
+    return returnPop(&s);
+}
+
+float result(ExpressionTree B) {
     if(B == NULL) return 0;
-
     if(is_digit(B->data)) return to_digit(B->data);
-
-    float left_result = result(B->left);
-    float right_result = result(B->right);
-
-    return operation(left_result , B->data , right_result);
+    
+    return operation(result(B->left) , B->data , result(B->right));
 }

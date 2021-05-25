18a16342d23508d72202dba1e6724e4050960739
OmarElKhatibCS <omarelkhatib@mail.com>
2021-05-25 15:27:15 +0300

    draw middle , and bottom part of the flag

--- I2207/project/palestine.asm
+++ I2207/project/palestine.asm
@@ -1,6 +1,6 @@
 # Bitmap Display Configuration:
-# - Unit width in pixels: 8
-# - Unit height in pixels: 8
+# - Unit width in pixels: 16
+# - Unit height in pixels: 16
 # - Display width in pixels: 512
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
@@ -14,25 +14,24 @@
 # $t3 stores the white colour code
 li $t0,BASE_ADDRESS
 li $t1,0xce1126
-li $t2,0x00ff00
+li $t2,0x007a3d
 li $t3,0xffffff
 
 # $t5 is a counter to decide what pixel should draw on next
 # increment by 4 on every loop
-li $t5,0
-drawTop:
-	sw $t1, 0($t0)
-	addi $t5,$t5,4
-	beq $t5,1028,drawMiddle
-	addi $t0,$t5,BASE_ADDRESS
-	j drawTop
+li $t5,640
 drawMiddle:
-	sw $t2, 0($t0)
+	addi $t0,$t5,BASE_ADDRESS
+	sw $t3 0($t0)
+	addi $t5,$t5,4
+	beq $t5,1408,drawBottom
+	j drawMiddle
+drawBottom:
 	addi $t0,$t5,BASE_ADDRESS
 	addi $t5,$t5,4
-	beq $t5,2052,end
-	j drawMiddle
-
+	sw $t2, 0($t0)
+	beq $t5,2048,end
+	j drawBottom
 end:
 	li $v0, 10 # gracefully shutdown
 	syscall
